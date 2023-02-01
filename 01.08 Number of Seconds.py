20afec1986d9eb5020fb0ef6956578b7ea767c76
jjohnson360 <jjohnson10@ualr.edu>
2023-02-01 22:10:06 +0000

    Number of Seconds

---
+++ 01.08 Number of Seconds.py
@@ -0,0 +1,6 @@
+minutes = int(input("Enter the number of minutes: "))
+seconds = int(input("Enter the number of seconds: "))
+
+total_seconds = minutes * 60 + seconds
+
+print("The total number of seconds is:", total_seconds)
