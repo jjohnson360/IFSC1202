2c4de136c263eb9b4a5df9d0060f3cbe0363100e
jjohnson360 <jjohnson10@ualr.edu>
2023-02-01 20:51:48 +0000

    Two Timestamps

--- 01.06 Two Timestamps.py
+++ 01.06 Two Timestamps.py
@@ -1,4 +1,16 @@
-timestamp1 = float("06.01.30: ")
-timestamp2 = float("06.02.10: ")
-differenace = timestamp2 - timestamp1
-print("seconds between timestamps:", difference)
+timestamp1 = (6 * 3600) + (1 * 60) + 30
+timestamp2 = (6 * 3600) + (2 * 60) + 10
+
+timestamp3 = (2 * 3600) + (3 * 60) + 4
+timestamp4 = (4 * 3600) + (7 * 60) + 28
+
+timestamp5 = (10 * 3600) + (8 * 60) + 2
+timestamp6 = (10 * 3600) + (8 * 60) + 2
+
+difference_1 = timestamp2 - timestamp1
+difference_2 = timestamp4 - timestamp3
+difference_3 = timestamp6 - timestamp5
+
+print("seconds between timestamps:", difference_1)
+print("seconds between timestamps:", difference_2)
+print("seconds between timestamps:", difference_3)
