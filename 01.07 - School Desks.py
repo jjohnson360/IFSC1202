d3aa4b342d7ffd4a66629a5eaae1a2ae7a4212c1
jjohnson360 <jjohnson10@ualr.edu>
2023-02-01 21:57:02 +0000

    School Desk

---
+++ 01.07 - School Desks.py
@@ -0,0 +1,19 @@
+a = int(input("Classroom A: "))
+b = int(input("Classroom B: "))
+c = int(input("Classroom C: "))
+
+d = int(input("Classroom A: "))
+e = int(input("Classroom B: "))
+f = int(input("Classroom C: "))
+
+g = int(input("Classroom A: "))
+h = int(input("Classroom B: "))
+i = int(input("Classroom C: "))
+
+total_1 = (a / 2) + (b // 2 + 1) + (c / 2)
+total_2 = (d / 2) + (e // 2 + 1) + (f / 2)
+total_3 = (g // 2 + 1) + (h / 2) + (i // 2 + 1)
+
+print(int(total_1))
+print(int(total_2))
+print(int(total_3))
