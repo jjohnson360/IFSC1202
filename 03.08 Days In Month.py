f944e3c63d78794192ea43d4d84f56d51570f307
jjohnson360 <jjohnson10@ualr.edu>
2023-02-08 04:44:13 +0000

    03.08 - Days In Month

---
+++ 03.08 Days In Month.py
@@ -0,0 +1,12 @@
+def days_in_month(month):
+    if month in [1, 3, 5, 7, 8, 10, 12]:
+        return 31
+    elif month in [4, 6, 9, 11]:
+        return 30
+    elif month == 2:
+        return 28
+    else:
+        return "Invalid month"
+
+month = int(input("Enter month: "))
+print(f"{days_in_month(month)}")
