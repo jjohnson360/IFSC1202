23e24452b00cd1a0ece0a062cf05647a396fd408
jjohnson360 <jjohnson10@ualr.edu>
2023-02-14 03:28:01 +0000

    04.06 - Squares In A Range

--- 04.06 Squares In a Range.py
+++ 04.06 Squares In a Range.py
@@ -2,7 +2,8 @@ def main():
     a = int(input("Enter A: ").strip())
     b = int(input("Enter B: ").strip())
     for i in range(a, b+1):
-        print("{}*2={}".format(i, i**2))
+        print("{}*{}={}".format(i, i, i * i))
+
 
 if __name__ == '__main__':
     main()
