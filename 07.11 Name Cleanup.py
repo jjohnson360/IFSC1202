d0aacd3af3d35c8ed1252faef65e0e929f2a2c0e
jjohnson360 <jjohnson10@ualr.edu>
2023-03-08 03:32:24 +0000

    07.11 - Project - Name Cleanup

--- 07.11 Name Cleanup.py
+++ 07.11 Name Cleanup.py
@@ -1,40 +1,16 @@
-def ProperCase(s):
-    if not s:
-        return ''
-    return s[0].upper() + s[1:].lower()
-
-def RemoveNewLine(s):
-    return s.replace('\n', '')
-
-def Trim(s):
-    return s.strip()
-
-def FirstName(s):
-    first_space = s.find(' ')
-    return ProperCase(s[:first_space])
-
-def LastName(s):
-    last_space = s.rfind(' ')
-    return ProperCase(s[last_space+1:])
-
-def MiddleName(s):
-    first_space = s.find(' ')
-    last_space = s.rfind(' ')
-    if first_space == -1 or last_space == -1 or last_space <= first_space:
-        return ''
-    middle_name = Trim(s[first_space:last_space])
-    middle_name = ProperCase(middle_name)
-    if len(middle_name) == 1:
-        middle_name += '.'
-    return middle_name
+def format_name(name):
+    first, *middle, last = name.strip().split()
+    middle = ' '.join(m.capitalize() for m in middle)
+    return f'{first.capitalize()} {middle} {last.capitalize()}'
 
 print('{:<12}{:<12}{:<12}'.format('First', 'Middle', 'Last'))
 print('-'*36)
 
 with open('07.11 Names.txt', 'r') as f:
     for line in f:
-        line = RemoveNewLine(Trim(line))
-        first_name = FirstName(line)
-        middle_name = MiddleName(line)
-        last_name = LastName(line)
-        print('{:<12}{:<12}{:<12}'.format(first_name, middle_name, last_name))
+        name = format_name(line)
+        first, *middle, last = name.split()
+        if middle:
+            print('{:<12}{:<12}{:<12}'.format(first, middle[0], last))
+        else:
+            print('{:<12}{:<12}{:<12}'.format(first, '', last))
