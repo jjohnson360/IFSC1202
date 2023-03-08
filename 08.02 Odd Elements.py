1d55326d0c01c0c0f875e7ecb8a96cff232972c9
jjohnson360 <jjohnson10@ualr.edu>
2023-03-08 03:51:33 +0000

    08.02 - Odd Elements

---
+++ 08.02 Odd Elements.py
@@ -0,0 +1,9 @@
+s = input("Enter Values Separated by Spaces: ")
+
+nums = []
+for num_str in s.split():
+    nums.append(int(num_str))
+
+for i in range(len(nums)):
+    if nums[i] % 2 == 1:
+        print(nums[i])
