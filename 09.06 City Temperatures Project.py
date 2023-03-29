6d67af0e559aeeb1bd3654a343908cd1f1f4fe84
jjohnson360 <jjohnson10@ualr.edu>
2023-03-29 18:15:30 +0000

    09.06 - Project - City Temperatures

--- 09.06 City Temperatures Project.py
+++ 09.06 City Temperatures Project.py
@@ -5,7 +5,6 @@ with open('09.06 CityTemps.txt', 'r') as file:
             row = line.strip().split()
             temps.append(row)
 
-# Calculate the Average Temperature for each location
 for i in range(len(temps)):
     total = 0
     for j in range(1, len(temps[i])):
@@ -13,7 +12,6 @@ for i in range(len(temps)):
     avg_temp = total // (len(temps[i])-1)
     temps[i].append(str(avg_temp))
 
-# Print the Results
 print('{:<8}{:<9}{:<9}{:<9}{:<9}{:<9}{:<9}{:<9}{:<9}'.format('City', 'Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa', 'Su', 'Avg'))
 for row in temps:
     print('{:<8}{:<9}{:<9}{:<9}{:<9}{:<9}{:<9}{:<9}{:<9}'.format(*row))
