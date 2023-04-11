240fd6d14e50f90e5dd50db616564134b66cd786
jjohnson360 <jjohnson10@ualr.edu>
2023-04-11 23:25:42 +0000

    10.03 - Car Class

---
+++ 10.03 Cars.py
@@ -0,0 +1,27 @@
+class Car:
+    def __init__(self, year, make):
+        self.year = year
+        self.make = make
+        self.speed = 0
+
+    def accelerate(self, value):
+        self.speed += value
+
+    def brake(self, value):
+        self.speed = max(0, self.speed - value)
+
+
+with open("10.03 Cars.txt", "r") as f:
+    year_make = f.readline().strip().split(",")
+    car = Car(year_make[0], year_make[1])
+
+    print("Make: {}\nYear: {}\n".format(car.make, car.year))
+    print("{:>8}    {:>5}".format("Change", "Speed"))
+    for line in f:
+        value = int(line.strip())
+        if value > 0:
+            car.accelerate(value)
+            print("{:>8}    {:>5}".format(value, car.speed))
+        else:
+            car.brake(-value)
+            print("{:>8}    {:>5}".format(value, car.speed))
