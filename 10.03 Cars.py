9d78b0ee662a4cd4a7ce44d765fc48e84474c77d
jjohnson360 <jjohnson10@ualr.edu>
2023-04-12 01:25:41 +0000

    10.03 - Car Class

--- 10.03 Cars.py
+++ 10.03 Cars.py
@@ -15,13 +15,13 @@ with open("10.03 Cars.txt", "r") as f:
     year_make = f.readline().strip().split(",")
     car = Car(year_make[0], year_make[1])
 
-    print("Make: {}\nYear: {}\n".format(car.make, car.year))
-    print("{:>8}    {:>5}".format("Change", "Speed"))
+    print("Make:{}\nYear: {}\n".format(car.make, car.year))
+    print("{:>0}    {:>5}".format("Change", "Speed"))
     for line in f:
         value = int(line.strip())
         if value > 0:
             car.accelerate(value)
-            print("{:>8}    {:>5}".format(value, car.speed))
+            print("{:>6}  {:>7}".format(value, car.speed))
         else:
             car.brake(-value)
-            print("{:>8}    {:>5}".format(value, car.speed))
+            print("{:>6}  {:>7}".format(value, car.speed))
